0ba578d77bdc0cfe5d903effe2196aa5685f9a94
FabianMatata <mfabian601@gmail.com>
2022-04-02 09:47:25 +0300

    order id

--- home/models.py
+++ home/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Customer(models.Model):
+    id = models.AutoField(primary_key=True)
     LOCATION_CHOICES = (('Nairobi', 'Nairobi'), ('Mombasa', 'Mombasa'), ('Kisumu', 'Kisumu'), ('Embu', 'Embu'), ('Nakuru', 'Nakuru'))
     pickup_location = models.CharField(max_length=100, choices=LOCATION_CHOICES)
     destination_location = models.CharField(max_length=100, choices=LOCATION_CHOICES)
@@ -10,7 +11,7 @@ class Customer(models.Model):
     receipient_name = models.CharField(max_length=200)
     receipient_contact = models.CharField(max_length=200)
     PACKAGE_CHOICES = (('Small', 'Small'), ('Large', 'Large'))
-    parcel_packages = models.CharField(max_length=100, choices=PACKAGE_CHOICES)
+    parcel_package = models.CharField(max_length=100, choices=PACKAGE_CHOICES)
 
     # sender_fname = models.CharField(max_length=200)
     # sender_lname = models.CharField(max_length=200)
