4f262a4db2afa64196f4c36b94dc74a34322cbbd
feLycks <felix.obiero@student.moringaschool.com>
2022-04-05 06:50:16 +0300

    reg file clean code

--- register/views.py
+++ register/views.py
@@ -1,10 +1,15 @@
 import django
-from django.shortcuts import render
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
 
 # Create your views here.
 
-def register(request):
-    form = UserCreationForm
-    return render(request, "register/register.html", {"form":form})
+def register(response):
+    if response.method == 'POST':
+        form = RegisterForm(response.POST)
+        if form.is_valid:
+            form.save()
+        return redirect("/register/login.html")
+    else:
+        form = RegisterForm()
+    return render(response, "register/register.html", {"form":form})
