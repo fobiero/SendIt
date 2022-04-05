578ca354e85f51d63896d879f162247488100ec8
feLycks <felix.obiero@student.moringaschool.com>
2022-04-05 06:23:18 +0300

    created reg file

--- SendIt/urls.py
+++ SendIt/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
+from register import views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls'))
+    path('', include('home.urls')),
+    path('register', v.register, name='register'),
 ]
